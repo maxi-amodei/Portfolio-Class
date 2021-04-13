379a75a977a677b7ef3adc521c8f0359ec322664
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-13 15:04:57 -0300

    Showing profit of the portfolio with new profit method

--- portfolio.rb
+++ portfolio.rb
@@ -15,4 +15,26 @@ class Portfolio
     @stocks.delete(ticker)
   end
 
+  def position_value(date)
+    cost = 0
+    @stocks.each do |ticker, quantity|
+      stock = Stock.new(ticker)
+      
+      cost += (stock.price(date) * quantity)
+    end
+    
+    cost
+  end
+
+  def profit(start_date, end_date)
+    starting_value = position_value(start_date)
+    ending_value = position_value(end_date)
+    profit =  ending_value - starting_value
+    period_return = profit / starting_value
+    {
+      Profit: profit.round(2),
+      
+    }
+  end
+
 end
