ab2b6536da3ee0b68c523b64998fac3f8701aed8
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-13 14:59:58 -0300

    Creating stock class

---
+++ stock.rb
@@ -0,0 +1,15 @@
+
+
+class Stock
+  attr_reader :ticker
+
+  include  APIRequest
+  def initialize(ticker)
+    @ticker = ticker
+  end
+
+  def price(start_date, end_date = start_date)
+    data = fetch_price(@ticker,start_date, end_date)
+    data[0]["close"].to_f
+  end
+end
